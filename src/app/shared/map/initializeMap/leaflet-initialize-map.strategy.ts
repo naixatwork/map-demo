211943e0022f0329b5dfa3b1d6c6b94b5e6232a8
naixatwork <naixatwork@gmail.com>
2022-10-08 06:29:46 +0330

    feat(location): used map component

--- src/app/shared/map/initializeMap/leaflet-initialize-map.strategy.ts
+++ src/app/shared/map/initializeMap/leaflet-initialize-map.strategy.ts
@@ -5,7 +5,7 @@ export class LeafletInitializeMapStrategy implements InitializeMapBaseStrategy {
   initializeMap(targetElement: HTMLElement): Map {
     return map(targetElement, {
       center: [51.509865, -0.118092],
-      zoom: 10
+      zoom: 10,
     });
   }
 }
